a866d89fe4bd0863ba0617135d8cf65330470cb2
Rizki Ramadhan <rizkiramdhanx@gmail.com>
2024-12-29 20:09:15 +0700

    up

---
+++ revisi.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 27, 2024 at 01:14 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `library_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `cover` varchar(255) NOT NULL,
+  `quantity` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `genre`, `author`, `category`, `cover`, `quantity`) VALUES
+(11, 'Sequi irure mollit e', 'Sunt velit tenetur i', 'Quia quisquam qui qu', 'Similique in ut et p', '2022-04-13 052519.jpg', 517);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowed`
+--
+
+CREATE TABLE `borrowed` (
+  `id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `book_id` int NOT NULL,
+  `due_date` date NOT NULL,
+  `status` varchar(10) DEFAULT 'borrowed'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrowed`
+--
+
+INSERT INTO `borrowed` (`id`, `student_id`, `book_id`, `due_date`, `status`) VALUES
+(1, 1, 11, '2012-10-12', 'returned');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int NOT NULL,
+  `nim` varchar(15) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `contact` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `nim`, `name`, `contact`) VALUES
+(1, '98', 'Jermaine Parsons', '49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(2, 'zekuqifito', 'buvigyt@mailinator.com', '$2y$10$mgI5BpsjhYRaZym8nzMhm.9L9XW2/mDSv0wa1oSFGLzF8e38IZV0e'),
+(3, 'ambatron', 'rizkiramadhanx@gmail.com', '$2y$10$q2tIoFBZtjWtyiC8ZjcWMeND73FYGa8esO3yy7TTtvya1A9ShlyVm');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `borrowed`
+--
+ALTER TABLE `borrowed`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `borrowed`
+--
+ALTER TABLE `borrowed`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrowed`
+--
+ALTER TABLE `borrowed`
+  ADD CONSTRAINT `borrowed_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  ADD CONSTRAINT `borrowed_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
+COMMIT;
